e91289f72601cf001b3073577e9045cc88df820f
Александр Ефанов <verpl01@yandex.ru>
2020-05-02 03:10:28 +0500

    Создан класс создания коментария

---
+++ Application/Comments/Create.cs
@@ -0,0 +1,65 @@
+﻿using Application.Errors;
+using Application.Interfaces;
+using AutoMapper;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Comments
+{
+    public class Create
+    {
+        public class Command : IRequest<CommentDto>
+        {
+            public string Body { get; set; }
+            public Guid ActivityId { get; set; }
+            public string UserName { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command, CommentDto>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+
+            public async Task<CommentDto> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.ActivityId);
+                if (activity == null)
+                    throw new RestException(HttpStatusCode.NotFound, new { Activity = "Not found" });
+
+                var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == request.UserName);
+
+                var comment = new Comment
+                {
+                    Author = user,
+                    Activity = activity,
+                    Body = request.Body,
+                    CreatedAt = DateTime.Now
+                };
+
+                activity.Comments.Add(comment);
+
+                var success = await _context.SaveChangesAsync() > 0;
+
+                if (success)
+                    return _mapper.Map<CommentDto>(comment);
+
+                throw new Exception("Problem savimg changes");
+
+            }
+        }
+    }
+}

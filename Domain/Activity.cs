d7f6cddc8484e2e4f4a961687275714741f612a8
Александр Ефанов <verpl01@yandex.ru>
2020-05-02 02:47:05 +0500

    Добавлена модель Comment

--- Domain/Activity.cs
+++ Domain/Activity.cs
@@ -14,5 +14,6 @@ namespace Domain
         public string City { get; set; }
         public string Venue { get; set; }
         public virtual ICollection<UserActivity> UserActivities { get; set; }
+        public virtual ICollection<Comment> Comments { get; set; }
     }
 }
---
+++ Domain/Comment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class Comment
+    {
+        public Guid Id { get; set; }
+        public string Body { get; set; }
+        public virtual AppUser Author { get; set; }
+        public virtual Activity Activity { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+    }
+}

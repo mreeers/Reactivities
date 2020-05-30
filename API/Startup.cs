b42a59e418512856f0019bd6ed4e842ac4b28820
Александр Ефанов <verpl01@yandex.ru>
2020-05-31 02:42:01 +0500

    Adding a Profile reader helper class and interface

--- API/Startup.cs
+++ API/Startup.cs
@@ -21,6 +21,7 @@ using AutoMapper;
 using Infrastructure.Photos;
 using System.Threading.Tasks;
 using API.SignalR;
+using Application.Profiles;
 
 namespace API
 {
@@ -108,6 +109,7 @@ namespace API
             services.AddScoped<IJwtGenerator, JwtGenerator>();
             services.AddScoped<IUserAccessor, UserAccessor>();
             services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+            services.AddScoped<IProfileReader, ProfileReader>();
             services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
         }
 
--- Application/Profiles/Profile.cs
+++ Application/Profiles/Profile.cs
@@ -1,4 +1,5 @@
 ﻿using Domain;
+using Newtonsoft.Json;
 using System.Collections.Generic;
 
 namespace Application.Profiles
@@ -9,6 +10,10 @@ namespace Application.Profiles
         public string UserName { get; set; }
         public string Image { get; set; }
         public string Bio { get; set; }
+        [JsonProperty("following")]
+        public bool IsFollowed { get; set; }
+        public int FollowersCount { get; set; }
+        public int FollowingCount { get; set; }
         public virtual ICollection<Photo> Photos { get; set; }
     }
 }

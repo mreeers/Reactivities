c23802680630bd993512666ff59f48d4a499dcdf
Александр Ефанов <verpl01@yandex.ru>
2020-05-31 00:15:12 +0500

    Adding the UserFollower Entity

---
+++ Domain/UserFollowing.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class UserFollowing
+    {
+        public string ObserverId { get; set; }
+        public virtual AppUser Observer { get; set; }
+        public string TargetId { get; set; }
+        public virtual AppUser Target { get; set; }
+    }
+}

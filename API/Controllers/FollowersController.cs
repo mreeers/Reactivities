7c12433ba67f80b266d614d6e658d117a35a60b5
Александр Ефанов <verpl01@yandex.ru>
2020-05-31 03:13:50 +0500

    Getting a List of Followings for a user

--- API/Controllers/FollowersController.cs
+++ API/Controllers/FollowersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Application.Followers;
+using Application.Profiles;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,12 @@ namespace API.Controllers
         public async Task<ActionResult<Unit>> Unfollow(string username)
         {
             return await Mediator.Send(new Delete.Command { Username = username });
+        }
+
+        [HttpGet("{username}/follow")]
+        public async Task<ActionResult<List<Profile>>> GetFollowings(string username, string predicate)
+        {
+            return await Mediator.Send(new List.Query { Username = username, Predicate = predicate });
         }
     }
 }
---
+++ Application/Followers/List.cs
@@ -0,0 +1,66 @@
+﻿using Application.Profiles;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Followers
+{
+    public class List
+    {
+        public class Query : IRequest<List<Profile>>
+        {
+            public string Username { get; set; }
+            public string Predicate { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Query, List<Profile>>
+        {
+            private readonly DataContext _context;
+            private readonly IProfileReader _profileReader;
+
+            public Handler(DataContext context, IProfileReader profileReader)
+            {
+                _context = context;
+                _profileReader = profileReader;
+            }
+
+            public async Task<List<Profile>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var queryble = _context.Followings.AsQueryable();
+
+                var userFollowings = new List<UserFollowing>();
+                var profiles = new List<Profile>();
+
+                switch (request.Predicate)
+                {
+                    case "followers":
+                        {
+                            userFollowings = await queryble.Where(x => x.Target.UserName == request.Username).ToListAsync();
+                            foreach(var follower in userFollowings)
+                            {
+                                profiles.Add(await _profileReader.ReadProfile(follower.Observer.UserName));
+                            }
+                            break;
+                        }
+                    case "following":
+                        {
+                            userFollowings = await queryble.Where(x => x.Observer.UserName == request.Username).ToListAsync();
+                            foreach (var follower in userFollowings)
+                            {
+                                profiles.Add(await _profileReader.ReadProfile(follower.Target.UserName));
+                            }
+                            break;
+                        }
+                }
+                return profiles;
+            }
+        }
+    }
+}

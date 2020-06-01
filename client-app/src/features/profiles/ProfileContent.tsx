c1f5c61acd21ce2ca741eaa334af113eaa07d925
Александр Ефанов <verpl01@yandex.ru>
2020-06-01 23:14:51 +0500

    Getting a list of followings from the API

--- client-app/src/features/profiles/ProfileContent.tsx
+++ client-app/src/features/profiles/ProfileContent.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import {Tab} from "semantic-ui-react";
 import ProfilePhotos from "./ProfilePhotos";
 import ProfileDescription from "./ProfileDescription";
+import ProfileFollowings from "./ProfileFollowings";
 
 const panes = [
   {menuItem: 'About', render: () => <ProfileDescription/>},
   {menuItem: 'Photos', render: () => <ProfilePhotos/>},
   {menuItem: 'Activities', render: () => <Tab.Pane>Activities content</Tab.Pane>},
-  {menuItem: 'Followers', render: () => <Tab.Pane>Followers content</Tab.Pane>},
-  {menuItem: 'Following', render: () => <Tab.Pane>Following content</Tab.Pane>},
+  {menuItem: 'Followers', render: () => <ProfileFollowings />},
+  {menuItem: 'Following', render: () => <ProfileFollowings />},
 ];
 
 const ProfileContent: React.FC = () => {
---
+++ client-app/src/features/profiles/ProfileFollowings.tsx
@@ -0,0 +1,34 @@
+import React, {useContext, useEffect} from "react";
+import {RootStoreContext} from "../../app/stores/rootStore";
+import {Tab, Grid, Header, Card} from "semantic-ui-react";
+import ProfileCard from "./ProfileCard";
+
+const ProfileFollowings = () => {
+    const rootStore = useContext(RootStoreContext);
+    const {profile, followings, loadFollowings, loading} = rootStore.profileStore;
+
+    useEffect(() => {
+        loadFollowings('following')
+    }, [loadFollowings]);
+
+    return (
+        <Tab.Pane loading={loading}>
+            <Grid>
+                <Grid.Column width={16}>
+                    <Header floated={'left'} icon={'user'}
+                            content={true ? `People following ${profile!.displayName}` : `People ${profile!.displayName} is following`}/>
+                </Grid.Column>
+                <Grid.Column width={16}>
+                    <Card.Group itemsPerRow={5}>
+                        {followings.map((profile) => (
+                            <ProfileCard key={profile.userName} profile={profile} />
+                        ))}
+                    </Card.Group>
+
+                </Grid.Column>
+            </Grid>
+        </Tab.Pane>
+    )
+};
+
+export default ProfileFollowings;
